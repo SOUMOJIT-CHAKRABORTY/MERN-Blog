12e75e65719b026f38a98cefaea6d5ff774aebfd
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-01-22 03:11:08 +0530

    project created and packages are installed

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main className="p-2.5 max-w-[500px] mx-auto my-0">
+      <header className="flex justify-between">
+        <a href=""> MyBlog</a>
+        <nav className="space-x-4">
+          <a href="">Login </a>
+          <a href="">Register </a>
+        </nav>
+      </header>
+    </main>
+  );
+}

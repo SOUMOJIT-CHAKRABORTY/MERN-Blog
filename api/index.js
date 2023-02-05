42ba109f12116bcb021927404acaf89c5825f692
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-02-06 03:45:13 +0530

    got connection error

--- api/index.js
+++ api/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/Users");
 const app = express();
 
 app.use(express.json());
 
+// mongoose.connect(
+//   "mongodb+srv://blog:sbDfdGiNqIw8fbE7@cluster0.soavyro.mongodb.net/?retryWrites=true&w=majority"
+// );
+
 app.use(cors());
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  const userDoc = await User.create({
+    username,
+    password,
+  });
+  res.json(userDoc);
 });
 
 app.listen(4000);
+
+//
